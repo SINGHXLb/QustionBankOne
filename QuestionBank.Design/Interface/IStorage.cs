8c44a422303ef595043ec65346a9ebb7a33e42b3
pushpe <pushpendra.patwal@ihis.com.sg>
2021-10-30 22:53:26 +0800

    Clean up

---
+++ QuestionBank.Design/Interface/IStorage.cs
@@ -0,0 +1,22 @@
+﻿using QuestionBank.Design.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuestionBank.Design.Interface
+{
+    interface IStorage
+    {
+                
+        
+        //Question 
+        bool add(Question doc);
+        bool update(Question doc);
+        bool remove(String docId);
+        bool commit();
+        Question get(String questionId);        
+        //
+
+
+    }
+}
---
+++ QuestionBank.Design/Model/Question.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuestionBank.Design.Model
+{
+    class Question
+    {
+    }
+}
